3ebdca3734f97485af429fd990cae87e40cce983
Andrés Celis <andcls98@gmail.com>
2026-01-04 23:15:18 -0600

    cambios 1.0

--- js/app.js
+++ js/app.js
@@ -271,3 +271,12 @@ menuBtn.addEventListener("click", () => {
   ];
   autoRecalcIds.forEach((id) => $(id)?.addEventListener("change", () => calc()));
 })();
+
+// Servicios animacion
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    card.style.setProperty("--x", `${e.clientX - rect.left}px`);
+    card.style.setProperty("--y", `${e.clientY - rect.top}px`);
+  });
+});
